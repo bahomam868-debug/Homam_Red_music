f31f561bef87031ae00a3b2ae11c786b8f195903
bahomam868-debug <bahomam868@gmail.com>
2026-08-20 19:00:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ const client = new Client({
 
 const distube = new DisTube(client, {
     plugins: [
+        new YouTubePlugin(),
         new SoundCloudPlugin(),
         new YtDlpPlugin({
             update: true
@@ -62,68 +63,9 @@ const distube = new DisTube(client, {
 
 console.log('🔴 RED MUSIC');
 console.log('✅ DisTube loaded successfully');
+console.log('✅ YouTube Plugin loaded successfully');
 console.log('✅ SoundCloud Plugin loaded successfully');
-console.log('✅ YouTube Yt-Dlp Plugin loaded successfully');
-
-// ======================================================
-// PACKAGE VERSIONS
-// ======================================================
-
-try {
-    console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━');
-    console.log('📦 PACKAGE VERSIONS');
-    console.log(
-        'DisTube:',
-        require('distube/package.json').version
-    );
-    console.log(
-        '@distube/youtube:',
-        require('@distube/youtube/package.json').version
-    );
-    console.log(
-        '@distube/yt-dlp:',
-        require('@distube/yt-dlp/package.json').version
-    );
-
-    try {
-        console.log(
-            '@distube/ytdl-core:',
-            require('@distube/ytdl-core/package.json').version
-        );
-    } catch {
-        console.log('@distube/ytdl-core: NOT INSTALLED');
-    }
-
-    try {
-        console.log(
-            '@distube/ytsr:',
-            require('@distube/ytsr/package.json').version
-        );
-    } catch {
-        console.log('@distube/ytsr: NOT INSTALLED');
-    }
-
-    console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━');
-
-} catch (error) {
-
-    console.error(
-        '❌ PACKAGE VERSION CHECK ERROR:',
-        error.message
-    );
-}
-
-
-// ======================================================
-// DATA
-// ======================================================
-
-const mode247 = new Map();
-const musicPanels = new Map();
-const savedPlaylists = new Map();
-const musicChannels = new Map();
-const manualLeave = new Set();
-
+console.log('✅ YtDlp Plugin loaded successfully');
 
 // ======================================================
 // HELPERS
